088350a7faa09dd14ca4a78b88e01e9baaeaa69d
pramilakhanal <pramilakhanal@pramilas-mbp.lan>
2018-12-05 11:57:06 -0700

    First Commit

---
+++ Edureka04/src/Day2/Variables.java
@@ -0,0 +1,42 @@
+package Day2;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+     int firstNumber;
+		
+		firstNumber = 4500;
+		
+		System.out.println("First Number :" +firstNumber);
+		
+			byte secondNumber = 78;
+			
+			long thirdNumber = 74832554351l;
+			
+			int forthNumber = 7458934;
+			
+			boolean flag;
+			
+			flag = false;
+			
+			char charVar = 'u';
+			
+			double myDoubleVar = 837289.743982;
+			
+			System.out.println(myDoubleVar);
+			
+			float myFloatVar = 48.824f;
+			System.out.println(myFloatVar);
+			
+			String str = "My name is Pramila";
+			System.out.println(str);
+		
+		}
+
+
+
+
+
+	}
+
---
+++ Edureka04/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author pramilakhanal
+ *
+ */
+module Edureka04 {
+}
